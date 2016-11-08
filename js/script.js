127c20be20e33c6ed13064e3febd98c4341e1533
nix07 <nikhil07prakash@gmail.com>
2016-11-08 17:48:22 +0530

    let the game begin

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-alert("678");
 $(document).ready(function() {
 	var x = "x";
 	var o = "o";
+	var player1 = 0;
+	var player2 = 0;
 	var turns = 0;
 
 	var spot1 = $('#spot1');
@@ -14,7 +15,105 @@ $(document).ready(function() {
 	var spot8 = $('#spot8');
 	var spot9 = $('#spot9');
 
-	$('#board').click(function() {
-		alert(1);
+	$('#board li').on('click', function() {
+
+		if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+			spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+			spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+			spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+			spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+			spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o') 
+			){
+				alert('Winner: PLAYER1');
+				player1++;
+				$('#board li').text('+');
+				$('#board li').removeClass('disabled');
+				$('#board li').removeClass('o');
+				$('#board li').removeClass('x');
+			}
+		else if(spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
+			spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
+			spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
+			spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
+			spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
+			spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x') 
+			){
+				alert('Winner: PLAYER2');
+				player2++;
+				$('#board li').text('+');
+				$('#board li').removeClass('disabled');
+				$('#board li').removeClass('o');
+				$('#board li').removeClass('x');
+			}
+		else if(turns == 9){
+			alert('Tie Game');
+				$('#board li').text('+');
+				$('#board li').removeClass('disabled');
+				$('#board li').removeClass('o');
+				$('#board li').removeClass('x');
+				turns = 0;
+			}
+		else if($(this).hasClass('disabled')) {
+			alert('This is already filled');
+		}
+		else if(turns%2 == 0 ) {
+			turns++;
+			$(this).text(o);
+			$(this).addClass('disabled o');
+			if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+			spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+			spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+			spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+			spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+			spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o') 
+			){
+				 alert('Winner: PLAYER1');
+				player1++;
+				turns = 0;
+			} 
+		}
+			else{
+				turns++;
+			$(this).text(x);
+			$(this).addClass('disabled x');
+			if(spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
+			spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
+			spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
+			spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
+			spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
+			spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x') 
+			){
+				 alert('Winner: PLAYER2');
+				player2++;
+				turns = 0;
+			}
+		}
 	});
-		
+	$('#reset').click(function() {
+		if(player1 > player2){
+			var play = document.getElementById('count');
+			play.innerHTML = "<h1>Player 1 : " + player1 + "</h1>" + "<h2>Player 2: " + player2 + "</h2>";
+		}
+		else if(player1 < player2){
+			var play = document.getElementById('count');
+			play.innerHTML = "<h2>Player 1 : " + player1 + "</h2>" + "<h1>Player 2: " + player2 + "</h1>";
+		}
+		else {
+			var play = document.getElementById('count');
+			play.innerHTML = "<h2>Player 1 : " + player1 + "</h2>" + "<h2>Player 2: " + player2 + "</h2>";
+		}
+		$("#board li").text("+");
+		$("#board li").removeClass('disabled');
+		$("#board li").removeClass('o');
+		$("#board li").removeClass('x');
+		turns = 0;
+	})
+});
