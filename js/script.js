30fbde5caab62da26b3c4268f59a5d725f43ea62
nix07 <nikhil07prakash@gmail.com>
2017-11-29 23:28:24 +0530

    Modified UI

--- js/script.js
+++ js/script.js
@@ -1,296 +1,134 @@
-var theboard = new Board();
-var ai = new AI('O');
-var selected, opponent;
+// Initialize variables
+var constrInput, resetInp, move, No, returnVal;
+var playField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var moveScore = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-$(document).ready(function(){
-	$('button').click(function(){
-		$(this).addClass('seed');
-		selected = $(this).text();
-		opponent = selected === 'X' ? 'O' : 'X';
-		ai = new AI('X');
-		$('button').attr('disabled', 'true').removeClass('hover');
-
-		if(opponent === 'X') {
-			//Make the computer's move
-			var move = ai.getBestMove(theboard);
-			var moveStr = move.join('');
-			$('#'+moveStr).text('X').addClass('selected').unbind( "click" );
-			theboard.makeMove('X', move);
-		}
-
-		//Handles clicking on a spot, only active after selecting which piece to play
-	$('.spot').click(handleClicks);
-	});
-});
-
-function handleClicks() {
-		if($('#result').text())
-			$('#result').empty();
-
-		//Make the players move.
-		$(this).text(selected);
-		$(this).addClass('selected').unbind( "click" );
-		var spotid = $(this).attr('id');
-		spotid = spotid.split('');
-		theboard.makeMove(selected, [spotid[0], spotid[1]]);
-
-		//Ensure that it is not time for gameOver
-		var winner = theboard.checkForWin();
-		if(winner || theboard.isFull())
-			gameOver(winner);
-		else {
-			//Make the computer's move
-			var move = ai.getBestMove(theboard);
-			console.log(move);
-			var moveStr = move.join('');
-			$('#'+moveStr).text(opponent).addClass('selected').unbind( "click" );
-			theboard.makeMove(opponent, move);
-
-			//Check again for game over.
-			winner = theboard.checkForWin();
-			if(winner || theboard.isFull())
-				gameOver(winner);
-		}
+// Insert X/O
+function insertMove(input, numberInput) {
+	//place player
+	$(input).html("X");
+	playField[numberInput - 1] = "X";
+	$(input).attr("onclick", "");
+	if (checkWin("X", playField)) {
+		window.alert("You Win!");
+		resetGame();
+		return;
+	}
+	//check if there's no draw
+	if (!countMoves()) {
+		window.alert("Draw");
+		resetGame();
+		return;
+	}
+	//place opponent
+	makeScores(playField.slice(0,9), "O");
+	move = makeDecision();
+	$(move[0]).html("O");
+	playField[move[1]] = "O";
+	$(move[0]).attr("onclick", "");
+	if (checkWin("O", playField)) {
+		window.alert("You Lose!");
+		resetGame();
+	}
 }
 
-function gameOver(winner) {
-	//Notify the player of the status
-	if(winner)
-		$('#result').text(winner +' won the game!');
-	else
-		$('#result').text('The game was a draw!');
-
-	//Wait a second to show the move, then reset
-	setTimeout(function(){
-		//Reset the game
-		$('table').empty();
-		$('table').append("<tr id='row0' class='row'><td id='00' class='spot'> </td><td id='01' class='spot'> </td><td id='02' class='spot'> </td></tr><tr id='row1' class='row'><td id='10' class='spot'> </td><td id='11' class='spot'> </td><td id='12' class='spot'> </td></tr><tr id='row2' class='row'><td id='20' class='spot'> </td><td id='21' class='spot'> </td><td id='22' class='spot'> </td></tr>");
-		theboard = new Board();
-		$('.spot').click(handleClicks);
-
-		if(opponent === 'X') {
-			//Make the computer's move
-			var move = ai.getBestMove(theboard);
-			var moveStr = move.join('');
-			$('#'+moveStr).text(opponent).addClass('selected').unbind( "click" );
-			theboard.makeMove(opponent, move);
-		}
-	}, 1000);
+// Decision making algorithm
+// The first function searches for empty spaces and
+// then calls the fillField function on each empty
+// field to assess wether it's a good choice or not
+function makeScores(input,player) {
+	for (var i = 0; i < 9; i++) {
+			if (input[i] === 0) {
+				input[i] = player;
+				moveScore[i] = fillField(input.slice(0,9), player, 0);
+				if (checkWin(player, input) && player == "O") {moveScore[i]+=1000000;}
+				input[i] = 0;
+			}
+	 }
+	return;
 }
 
-//Implementation of the game AI
-function AI(seed) {
-	this.marker = seed;
-	this.opponent = seed == 'X' ? 'O' : 'X';
-	this.max = 10;
-	this.min = -10;
-
-	this.minimax = function(board, player) {
-		var bestScore = -10,
-				currScore = 0,
-				moves = board.getAvailableMoves();
-
-		//Base case for finding leaf nodes
-		if(board.turnCnt >= 9 || board.checkForWin() || !moves)
-			return this.evaluate(board);
-
-		//Maximize
-		if(player === this.marker) {
-			bestScore = this.min;
-			for(var move in moves) {
-				var newBoard = board.clone();
-				newBoard.makeMove(this.marker, moves[move]);
-				currScore = this.minimax(newBoard, this.opponent);
-				if(currScore > bestScore) {
-					bestScore = currScore;
-				}
-			}
-			return bestScore;
+// The second function is a recursive implementation of
+// the minimax algorithm
+function fillField(input, player) {
+	player == "X" ? player = "O" : player = "X";
+	var score = 0;
+	for (var i = 0; i < 9; i++) {
+		if (input[i] === 0) {
+				input[i] = player;
+				if (checkWin(player, input) && player == "X") {
+						score -= 10;
+						continue;
+					}
+				if (checkWin(player, input) && player == "O") {
+						score += 10;
+						continue;
+					}
+				if (player == "X") {
+							score += fillField(input.slice(0,9),"X");
+					} else {
+							score += fillField(input.slice(0,9),"O");
+					}
+				input[i] = 0;
 		}
-
-		//Minimize
-		if(player === this.opponent) {
-			bestScore = this.max;
-			for(var move in moves) {
-				var newBoard = board.clone();
-				newBoard.makeMove(this.opponent, moves[move]);
-				currScore = this.minimax(newBoard, this.marker);
-				if(currScore < bestScore) {
-					bestScore = currScore;
-				}
-			}
-			return bestScore;
-		}
-	};
-
-	//Gets the best move for this board configuration
-	this.getBestMove = function(board) {
-		var bestScore = this.min;
-		var currScore;
-		var bestMove = null;
-		var moves = board.getAvailableMoves();
-		var corners = [[0, 0], [0, 2], [2, 0], [2, 2]];
-		//Prunes a few options for the first few states
-		if(board.turnCnt === 0)
-			return [1, 1];
-		else if(board.turnCnt === 1 && board.gamestate[1][1] === '')
-			return [1, 1];
-		else if(board.turnCnt === 1)
-			return corners[Math.floor(Math.random() * 4)];
-
-		for(var move in moves) {
-			var newBoard = board.clone();
-			newBoard.makeMove(this.marker, moves[move]);
-			currScore = this.minimax(newBoard, this.opponent);
-			console.log('Current score: ' + currScore);
-			console.log('Current move: ' + moves[move]);
-			if(currScore > bestScore) {
-				bestScore = currScore;
-				bestMove = moves[move];
-			}
-		}
-		return bestMove;
-	};
-
-	//Evaluates the score for the board passed by checking each line
-	this.evaluate = function(board) {
-		var score = 0;
-
-		score += this.evaluateLine(board, 0, 0, 0, 1, 0, 2);  // row 0
-		score += this.evaluateLine(board, 1, 0, 1, 1, 1, 2);  // row 1
-		score += this.evaluateLine(board, 2, 0, 2, 1, 2, 2);  // row 2
-		score += this.evaluateLine(board, 0, 0, 1, 0, 2, 0);  // col 0
-		score += this.evaluateLine(board, 0, 1, 1, 1, 2, 1);  // col 1
-		score += this.evaluateLine(board, 0, 2, 1, 2, 2, 2);  // col 2
-		score += this.evaluateLine(board, 0, 0, 1, 1, 2, 2);  // diagonal
-		score += this.evaluateLine(board, 0, 2, 1, 1, 2, 0);  // alternate diagonal
-
-		return score;
-	};
-
-	//Scores the line by checking each cell for our marker, 1 point for 1, 10 point for 2, 100 for 3, opposite for opponent marker
-	this.evaluateLine = function(board, r1, c1, r2, c2, r3, c3) {
-		var score = 0;
-
-		//First cell
-		if(board.gamestate[r1][c1] === this.marker)
-			score = 1;
-		else if(board.gamestate[r1][c1] === this.opponent)
-			score = -1;
-
-		//Second cell
-		if(board.gamestate[r2][c2] === this.marker){
-			if(score == 1) //Cell 1 was my marker
-				score = 10;
-			else if (score === -1) // Cell 1 was my opponent
-				return 0;
-			else //Cell 1 was empty
-				score = 1;
-		}
-		else if(board.gamestate[r2][c2] === this.opponent){
-			if(score == -1) //Cell 1 was opponent
-				score = -10;
-			else if (score === 1) // Cell 1 was my marker
-				return 0;
-			else //Cell 1 was empty
-				score = -1;
-		}
-
-		//Final cell
-		if(board.gamestate[r3][c3] === this.marker){
-			if(score > 1) //Cell 1 and or 2 was my marker
-				score *= 10;
-			else if (score < 0) // Cell 1 and or 2 was my opponent
-				return 0;
-			else //Cell 1 and 2 are empty
-				score = 1;
-		}
-		else if(board.gamestate[r3][c3] === this.opponent){
-			if(score < 0) //Cell 1 and or 2 was my opponent
-				score *= 10;
-			else if (score > 1) // Cell 1 and or 2 was my marker
-				return 0;
-			else //Cell 1 and 2 are empty
-				score = -1;
-		}
-		return score;
-	};
+	}
+	return score;
 }
 
-//Implementation of the board object
-function Board() {
-	this.turnCnt = 0;
-	this.gamestate = [['','',''],
-										['','',''],
-										['','','']];
-	//Returns the open positions on the board as an array of points as [row, column] or [y, x]
-	this.getAvailableMoves = function() {
-		var moves = [];
-
-		for(var row in this.gamestate)
-			for(var col in this.gamestate[row])
-				if(this.gamestate[row][col] === '')
-					moves.push([row, col]);
-
-		return moves;
-	};
-
-	this.clone = 	function() {
-		var newBoard = new Board();
-
-		//Copy over the positions of X's and O's and the turn number to the cloned board
-		for(var row = 0; row < 3; row++)
-			for(var col = 0; col < 3; col++)
-				newBoard.gamestate[row][col] = this.gamestate[row][col];
-		newBoard.turnCnt = this.turnCnt;
-
-		return newBoard;
-	};
-
-	//Will take in the player making the move as well as an [y, x] array of where to place the player's marker
-	this.makeMove = function(player, point) {
-		var row = parseInt(point[0]);
-		var col = parseInt(point[1]);
-		this.gamestate[row][col] = player;
-		this.turnCnt++;
-	};
-
-	this.isFull = function() {
-		return this.turnCnt === 9;
-	};
-
-	this.checkForWin = function() {
-		var boardState = this.gamestate;
-		var winner;
-
-		//checking the diagonals
-		if(boardState[1][1] !== '' &&
-			 ((boardState[0][0] === boardState[1][1]
-				 && boardState[2][2] === boardState[1][1])
-				|| (boardState[0][2] === boardState[1][1]
-						&& boardState[2][0] === boardState[1][1]))) {
-			winner = boardState[1][1];
-			return winner;
+// The third function assesses the scoreboard, chooses the
+// empty spot with the highest possible minimax score, and
+// then clears the scoreboard and returns the best move
+function makeDecision() {
+		returnVal = moveScore.indexOf(Math.max.apply(null, moveScore));
+		while (playField[returnVal] !== 0) {
+			moveScore[returnVal] -=100000;
+			returnVal = moveScore.indexOf(Math.max.apply(null, moveScore));
 		}
-		else {
-			//Checking the horizontals
-			for(var row in boardState) {
-				if(boardState[row][0] !== '' &&
-					 boardState[row][0] === boardState[row][1]
-					 && boardState[row][2] === boardState[row][1]) {
-					winner = boardState[row][0];
-					return winner;
-				}
-			}
-			//Verticals
-			for(var col in boardState) {
-				if(boardState[0][col] !== '' &&
-					 boardState[0][col] === boardState[1][col]
-					 && boardState[1][col] === boardState[2][col]) {
-					winner = boardState[0][col];
-					return winner;
-				}
-			}
+		for (var e = 0; e < 9; e++) {
+			moveScore[e] = 0;
 		}
-	};
+		constrInput = "#button" + (returnVal+1).toString();
+		return [constrInput, returnVal];
+}
+
+// Just a function to count the amount of moves left
+function countMoves() {
+	var output = 0;
+	for (var i = 0; i <= 8; i++) {
+		if (playField[i] === 0) {
+			output += 1;
+		}
+	}
+	return output;
+}
+
+// check if there's a winner
+function checkWin(playChar, field) {
+	for (var i = 0; i <= 3; i++) {
+		if ((field[i] === playChar &&
+				field[i + 3] === playChar &&
+				field[i + 6] === playChar) ||
+			 (field[i] === playChar &&
+				field[i + (4 - i)] === playChar &&
+				field[i + (4 - i) + (4 - i)] === playChar)) {
+			return true;
+		}
+	}
+	if ((field[0] === playChar && field[1] === playChar &&
+			field[2] === playChar) || (field[6] === playChar &&
+			field[7] === playChar && field[8] === playChar)) {
+		return true;
+	}
+	return false;
+}
+
+// RESET function
+function resetGame() {
+	for (var i = 1; i<= 9; i++) {
+		resetInp = "#button" + i;
+		$(resetInp).attr("onclick", 'insertMove("' +
+			resetInp + '",' + i + ')');
+		$(resetInp).html("");
+		playField[i - 1] = 0;
+	}
 }
